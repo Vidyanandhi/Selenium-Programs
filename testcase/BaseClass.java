944325092ec917caaa6f352b8412757eb9f69492
Vidyanandhi <90943158+Vidyanandhi@users.noreply.github.com>
2021-10-29 20:24:08 +0530

    Add files via upload

---
+++ testcase/BaseClass.java
@@ -0,0 +1,47 @@
+package testcase;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public ChromeDriver driver;
+	
+	@Parameters({"username","password","url"})//parameter name matters; order doesn't matter
+	@BeforeMethod
+	public void preCondition(String uName, String pWord,String url) { //order matters; but arg name can be anything
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		System.out.println(driver);
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(uName);
+		driver.findElement(By.id("password")).sendKeys(pWord);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+
+	}
+	
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+
+	}
+	
+	
+	
+	
+
+}
